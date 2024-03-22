201da482c9e1d77856987a6bf42f38ef8f0dee9e
Hyerin Kim <kdjsanh0217@naver.com>
2024-03-22 18:00:24 +0900

    favicon , 웹사이트 타이틀 변경

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@
 import './App.css';
 import {Routes,Route} from 'react-router-dom'
 import MainPage from './Pages/HomePage/Main/MainPage';
-import BoardPage from './Pages/Board/BoardPage';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
@@ -10,7 +9,6 @@ function App() {
   return (
     <Routes>
       <Route path='/' element = {<MainPage/>} />
-      {/* <Route path='/board' element= {<BoardPage/>}/> */}
     </Routes>
   );
 }
