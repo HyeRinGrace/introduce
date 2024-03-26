200f4f34d56d71421a5ba8a852737205286328a4
Hyerin Kim <kdjsanh0217@naver.com>
2024-03-26 19:27:09 +0900

    이미지 변경

--- src/Pages/HomePage/Main/Image/Image.jsx
+++ src/Pages/HomePage/Main/Image/Image.jsx
@@ -1,11 +1,10 @@
 import React from 'react'
-import me from '../../../../assets/me.jpeg'
 import './Image.css';
 
 const Image = () => {
   return (
     <div>
-        <img className = "meImage" src={me}/>
+        <img className = "meImage" src='https://i.ibb.co/C24cqx1/X.png'/>
     </div>
   )
 }
