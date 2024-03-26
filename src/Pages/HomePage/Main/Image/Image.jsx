33e41c0f0e7718f2769867160f5b7e7c8d794bf8
Hyerin Kim <kdjsanh0217@naver.com>
2024-03-26 20:44:01 +0900

    프로필이미지 수정

--- src/Pages/HomePage/Main/Image/Image.jsx
+++ src/Pages/HomePage/Main/Image/Image.jsx
@@ -5,7 +5,7 @@ import { Fade } from 'react-awesome-reveal';
 const Image = () => {
   return (
     <Fade duration={6000}>
-        <img className = "meImage" src='https://i.ibb.co/hgkbWtD/X.png'/>
+        <img className = "meImage" src='https://i.ibb.co/WPNxWnW/image.png'/>
     </Fade>
   )
 }
