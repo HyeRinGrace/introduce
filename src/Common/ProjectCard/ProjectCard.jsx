9a93ed1dfbd36e973d08714328fd7abfd861b268
Hyerin Kim <kdjsanh0217@naver.com>
2024-03-26 17:35:46 +0900

    인트로 폰트사이즈 적용

--- src/Common/ProjectCard/ProjectCard.jsx
+++ src/Common/ProjectCard/ProjectCard.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Card, Container, Row, Col } from 'react-bootstrap';
+import { Card, Container, Row, Col, Spinner } from 'react-bootstrap';
 import { Fade } from "react-awesome-reveal";
 import '../../Common/ProjectCard/ProjectCard.css';
 import ProjectModal from '../ProjectModal';
@@ -10,6 +10,7 @@ const ProjectCard = () => {
     const items = Projects.projects;
     const [showModal, setShowModal] = useState(false); // 모달 상태
     const [selectedProject, setSelectedProject] = useState(null); // 선택된 프로젝트
+    const [imageLoading, setImageLoading] = useState(true); // 이미지 로딩 상태
 
     // 클릭한 카드의 정보를 선택된 프로젝트로 설정하는 함수
     const handleCardClick = (project) => {
@@ -38,11 +39,17 @@ const ProjectCard = () => {
                         {items.map((item) => (
                             <Col key={item.id} md={3} style={{ marginBottom: '20px' }}>
                                 <Card className='Card'>
-                                    <Card.Img onClick={() => handleCardClick(item)} variant="top" src={item.imageURL} />
+                                    {imageLoading && <Spinner animation="border" />}
+                                    <Card.Img
+                                        onClick={() => handleCardClick(item)}
+                                        variant="top"
+                                        src={item?.imageURL}
+                                        onLoad={() => setImageLoading(false)} // 이미지 로드 성공 시 상태 업데이트
+                                        onError={() => setImageLoading(false)} // 이미지 로드 실패 시 상태 업데이트
+                                    />
                                     <Card.Body>
-                                        <Card.Title style={{ fontSize: '13px' }}>{item.title}</Card.Title>
+                                        <Card.Title style={{ fontSize: '13px' }}>{item?.title}</Card.Title>
                                     </Card.Body>
-                                    
                                 </Card>
                             </Col>
                         ))}
--- src/Pages/HomePage/Main/Context/Intro.jsx
+++ src/Pages/HomePage/Main/Context/Intro.jsx
@@ -1,39 +1,33 @@
-import React from 'react'
-import {Col} from 'react-bootstrap'
-import Typewriter from "typewriter-effect";
+// Intro.js
+import React from 'react';
+import { Col } from 'react-bootstrap';
+import Typewriter from 'typewriter-effect';
 import '../Context/intro.css';
 
 const Intro = () => {
   return (
-    <Col className='NameTag' style={{
-        width:'100px',
-        fontSize:'20px',
-        fontWeight:'bolder',
-        whiteSpace: 'nowrap'
-    }}>
-    <Typewriter
+    <Col className='NameTag'>
+      <Typewriter
         className="Motion"
         options={{
-        strings: ["인생은 늘 배움의 연속이다.","프론트엔드 개발자를 꿈꾸는 김혜린입니다."],
-        autoStart: true,
-        loop: true,
-        delay: 150,
+          strings: ["인생은 늘 배움의 연속이다.", "프론트엔드 개발자를 꿈꾸는 김혜린입니다."],
+          autoStart: true,
+          loop: true,
+          delay: 150,
         }}
-        style={{ fontSize: "60px" }}
+        style={{ fontSize: "80px" }}
       />
-    <br/>
-    <Typewriter
-          onInit={(typewriter) => {
-            typewriter
-              .typeString("KIM HYE RIN")
-              .pauseFor(1000)
-              .start();
-          }}
-        />
-      
-
+      <br />
+      <Typewriter
+        onInit={(typewriter) => {
+          typewriter
+            .typeString("KIM HYE RIN")
+            .pauseFor(1000)
+            .start();
+        }}
+      />
     </Col>
-  )
-}
+  );
+};
 
-export default Intro
+export default Intro;
